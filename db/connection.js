b7a22191ef4ed849a423848b667a28e8ae0e05e7
Laxmi-999 <regmilaxmi325@gmail.com>
2025-06-06 18:16:33 +0545

    enviroment variable added

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,14 @@
+require('dotenv').config(); // Load .env variables
 const mongoose = require('mongoose');
 
-
 async function main() {
-    try {
-      await mongoose.connect('mongodb://localhost:27017/ChatApp', );
-      console.log('Database connected');
-    } 
-    catch (error) {
-      console.error('Error connecting to database:', error);
-      process.exit(1); // Exit the process if unable to connect to the database
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    process.exit(1);
   }
-  main().catch(err => console.error(err));
+}
+
+main().catch(err => console.error(err));
